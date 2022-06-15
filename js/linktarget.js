503d50c4a8c2ece650d81844bde6f07f03f354b9
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2022-06-15 17:33:42 +0600

    price table link target

---
+++ js/linktarget.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll('[data-package-target]');
+const tabContents = document.querySelectorAll('[data-package-content]');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', (e) => {
+    tabs.forEach((tab) => {
+      tab.classList.remove('active'); // 3
+    });
+
+    const target = document.querySelector(tab.dataset.packageTarget); // 1
+    tabContents.forEach((content) => {
+      content.classList.remove('active'); //3
+    });
+
+    target.classList.add('active'); // 2
+    tab.classList.add('active'); // 2
+  });
+});
