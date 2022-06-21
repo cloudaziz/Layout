a35e92ffa4e674501d015dd2b1af361df2f37e3d
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2022-06-21 15:32:36 +0600

    update service layout

--- js/linktarget.js
+++ js/linktarget.js
@@ -4,15 +4,15 @@ const tabContents = document.querySelectorAll('[data-package-content]');
 tabs.forEach((tab) => {
   tab.addEventListener('click', (e) => {
     tabs.forEach((tab) => {
-      tab.classList.remove('active'); // 3
+      tab.classList.remove('active'); //
     });
 
     const target = document.querySelector(tab.dataset.packageTarget); // 1
     tabContents.forEach((content) => {
-      content.classList.remove('active'); //3
+      content.classList.remove('active'); // 3
     });
 
     target.classList.add('active'); // 2
-    tab.classList.add('active'); // 2
+    tab.classList.add('active'); //
   });
 });
